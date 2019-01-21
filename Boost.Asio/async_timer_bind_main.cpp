403cb5fd1125f857a60286c07a1aa66d1fbe44d3
dimitarpg13 <dimitar_pg13@hotmail.com>
2019-01-21 09:44:36 -0500

    adding async timer ex

---
+++ Boost.Asio/async_timer_bind_main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+void print(const boost::system::error_code& /*e*/,
+	   boost::asio::deadline_timer* t, int* count)
+{
+   std::cout << "Hello, world from timer callback!" << std::endl;
+   if (*count < 5) {
+       std::cout << *count << std::endl;
+       ++(*count);
+       t->epires_at(t->expires_at() + boost::posix_time::seconds(1));
+       t->async_wait(boost::bind(print, 
+		 boost::asio::placeholders::error, t, count));
+   }
+}
+
+int main (int argc, char* argv[]) {
+    boost::asio::io_service io;
+    boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+    t.async_wait(bost::bind(print, 
+	  boost::asio::placeholders::error, &t, &count));
+
+    io.run();
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
