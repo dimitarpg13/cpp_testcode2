cbd534b8950c256851c868a4bc72d3577134e42b
dimitarpg13 <dimitar_pg13@hotmail.com>
2019-02-09 18:17:08 -0500

    simple tcp server

--- epoll/epoll_example.c
+++ epoll/epoll_example.c
@@ -3,18 +3,19 @@
 #include <sys/epoll.h> // for epoll_create(), epoll_ctl(), struct epoll_event
 #include <string.h>
 
+#define MAX_EVENTS 5
+#define READ_SIZE 10
+
 int main(int argc, char *argv[]) {
-   struct epoll_event event;
+   int running = 1, event_count, i;
+   size_t bytes_read;
+   char read_buffer[READ_SIZE + 1];
+   struct epoll_event event, events[MAX_EVENTS];
    int epoll_fd = epoll_create1(0);
+
    if (epoll_fd == -1)
    {
       fprintf(stderr, "Failed to create epoll file descriptor\n");
-      return 1;
-   }
-
-   if (close(epoll_fd))
-   {
-      fprintf(stderr, "Failed to close epoll file descriptor\n");
       return 1;
    }
 
@@ -26,6 +27,25 @@ int main(int argc, char *argv[]) {
        fprintf(stderr, "Failed to add file description to epoll\n");
        close(epoll_fd);
        return 1;
+   }
+
+   while (running) 
+   {
+       printf("\nPolling for input...\n");
+       event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
+       printf("%d ready events\n", event_count);
+       for (i = 0; i < event_count; i++)
+       {
+           printf("Reading file descriptor '%d' -- ", events[i].data.fd);
+           bytes_read = read(events[i].data.fd, read_buffer, READ_SIZE);
+           printf("%zd bytes read.\n", bytes_read);
+           read_buffer[bytes_read] = '\0';
+           printf("Read '%s\n", read_buffer);
+
+           if (!strncmp(read_buffer, "stop\n", 5))
+               running = 0;
+       }
+
    }
 
    if (close(epoll_fd))
