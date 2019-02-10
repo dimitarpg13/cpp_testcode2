5f73d7e108e2b9f22fb9c2096cb5c8b18681b53b
dimitarpg13 <dimitar_pg13@hotmail.com>
2019-02-09 20:42:02 -0500

    simple tcp server

--- epoll/simple_tcp_server.c
+++ epoll/simple_tcp_server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <strings.h> /* bzero */
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,7 +14,8 @@ void error(char *msg)
 
 int main(int argc, char * argv[])
 {
-   int sockfd, newsockfd, portno, clilen, n;
+   socklen_t clilen;
+   int sockfd, newsockfd, portno, n;
    char buffer[256];
    struct sockaddr_in serv_addr, cli_addr;
   
@@ -42,8 +44,10 @@ int main(int argc, char * argv[])
 
    bzero(buffer,256);
    n = read(newsockfd, buffer, 255);
+   if (n < 0) error("ERROR reading from socket");
+   printf("Here is the message: %s", buffer);
    
-
-   
-
+   n = write(newsockfd, "I got your message", 18);
+   if (n < 0) error("ERROR writing to socket");
+   return 0; 
 }
